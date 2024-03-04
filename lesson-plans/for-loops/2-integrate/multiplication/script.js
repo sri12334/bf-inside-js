9aac4fdad160f81a0f9d90cbbb8bcfe5df20a2ec
sri12334 <srir48747@gmail.com>
2024-03-04 13:22:10 +0100

    isolate is finished

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -1,11 +1,33 @@
 import { readNumber, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('do-math').addEventListener('click', () => {
   debugger;
 
   // read user values
+  let left = Number(readNumber('left'));
+  let right = Number(readNumber('right'));
 
-  // use a for loop to multiply the two numbers
+  // initialize product variable
+  let product = 0;
 
+  // handle multiplication for positive 'right'
+  if (right > 0) {
+    for (let i = 0; i < left; i++) {
+      product += right;
+    }
+  }
+  // handle multiplication for negative 'right'
+  else if (right < 0) {
+    // if both left and right are negative, set product to 0
+    if (left < 0) {
+      product = 0;
+    } else {
+      for (let i = 0; i > right; i--) {
+        product -= left;
+      }
+    }
+  }
+ 
   // display the product
+   display('product', product);
 });
